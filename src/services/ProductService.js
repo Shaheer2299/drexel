a5d8f4872c8fc38341d04492337d1a615310c0c0
Shaheer2299 <72572695+Shaheer2299@users.noreply.github.com>
2020-12-27 19:22:26 +0500

    Update ProductService.js

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -4,7 +4,7 @@ class ProductsService extends GenericService {
   addProduct = (data) => this.post("products/add", data);
   deleteProduct = (_id) => this.delete("products/delete/"+_id);
   updateProduct = (_id, data) => this.put("products/api/" + _id, data);
-  getProducts = () => this.get("products");
+  getProducts = () => this.get("products/api");
   getSingleProduct = (id) => this.get("products/"+ id);
 }
 
